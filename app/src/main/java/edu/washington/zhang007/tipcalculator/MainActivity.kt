674ae4d7b1d5db8cfa2ebc04ab126a2a11946fe9
Henry Zhang <henry.zhang@live.com>
2018-04-16 01:09:20 -0700

    Ensure button only works if there is input

--- app/src/main/java/edu/washington/zhang007/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/washington/zhang007/tipcalculator/MainActivity.kt
@@ -2,11 +2,13 @@ package edu.washington.zhang007.tipcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.DecimalFormat
+import android.text.Editable
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +20,23 @@ class MainActivity : AppCompatActivity() {
         val tip: Button = btn_tip as Button
         val amount: EditText = txt_amount as EditText
 
-        tip.setOnClickListener {
+        // tip must not be clickable upon launch
+        tip.isEnabled = false
 
+        amount.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // if the string isn't "" return true
+                tip.isEnabled = (amount.text.toString() != "")
+            }
+        })
+
+        tip.setOnClickListener {
             val tipAmt = formatDecimal(amount.text.toString().toDouble() * .15)
 
             Toast.makeText(this, "You should tip: \$$tipAmt", Toast.LENGTH_SHORT).show()
