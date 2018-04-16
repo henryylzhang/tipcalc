68f36791828c4d0878b4ea4398e10dba67b85083
Henry Zhang <henry.zhang@live.com>
2018-04-16 00:01:46 -0700

    Format tip amount

--- app/src/main/java/edu/washington/zhang007/tipcalculator/MainActivity.kt
+++ app/src/main/java/edu/washington/zhang007/tipcalculator/MainActivity.kt
@@ -2,11 +2,32 @@ package edu.washington.zhang007.tipcalculator
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // UI declarations
+        val tip: Button = btn_tip as Button
+        val amount: EditText = txt_amount as EditText
+
+        tip.setOnClickListener {
+
+            val tipAmt = formatDecimal(amount.text.toString().toDouble() * .15)
+
+            Toast.makeText(this, "You should tip: \$$tipAmt", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun formatDecimal(value: Double): String {
+        val df = DecimalFormat("#.##")
+        return df.format(java.lang.Double.valueOf(value))
     }
 }
